bb8a00e32b300c7b6f02e22b9b990530dd43f693
danillo <danillosouza1704@gmail.com>
2022-03-12 19:07:42 -0300

    Criado novo tamanho de Frame

--- front/src/components/Frame/index.tsx
+++ front/src/components/Frame/index.tsx
@@ -5,13 +5,15 @@ type FrameTypes = {
   image: string;
   eliminated?: boolean;
   style?: React.CSSProperties;
+  size?: 'small' | 'large'
 };
 
-const Frame = ({ image, eliminated = false, style }: FrameTypes) => {
+const Frame = ({ image, eliminated = false, size = 'small', style }: FrameTypes) => {
+  const classNameFrame = size === 'large' ? 'largeFrame' : 'smallFrame'
   return (
     <div className="frame" style={style}>
-      <div style={{ opacity: eliminated ? 0.5 : 0 }} />
-      <img alt="" src={image} />
+      <div style={{ opacity: eliminated ? 0.5 : 0 }} className={classNameFrame}/>
+      <img alt="" src={image} className={classNameFrame}/>
     </div>
   );
 };
