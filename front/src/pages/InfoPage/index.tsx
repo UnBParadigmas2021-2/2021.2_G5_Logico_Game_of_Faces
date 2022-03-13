732bd3ae7605b9c86e73c68225d9553716768d09
danillo <danillosouza1704@gmail.com>
2022-03-12 21:30:19 -0300

    Adicionando tipo Styles

--- front/src/pages/InfoPage/index.tsx
+++ front/src/pages/InfoPage/index.tsx
@@ -1,32 +1,34 @@
 import React from "react";
-import { Button, Title, Caption } from '../../components'
+import { Button, Title, Caption } from "../../components";
+import { Styles } from "../../utils/Styles";
 
-const IMAGE_BACKGROUND = "background.webp"; 
+const IMAGE_BACKGROUND = "background.webp";
 const { innerHeight, innerWidth } = window;
 
 type InfoPageTypes = {
-  title?: string,
-	caption?: string;
-	onClick?: () => void;
-	buttonLabel?: string;
+  title?: string;
+  caption?: string;
+  onClick?: () => void;
+  buttonLabel?: string;
 };
 
-const InfoPage = ({title, caption, onClick, buttonLabel}:InfoPageTypes) => {
-
+const InfoPage = ({ title, caption, onClick, buttonLabel }: InfoPageTypes) => {
   const renderText = () => {
-    if(title)
-      return (
-        <Title label={title} style={styles.titlePosition as React.CSSProperties}/>
-      )
-    return <Caption label={caption} style={styles.captionPosition as React.CSSProperties}/>
-  }
+    if (title) return <Title label={title} style={styles.titlePosition} />;
+    return <Caption label={caption} style={styles.captionPosition} />;
+  };
 
   return (
     <React.Fragment>
-			<div style={styles.mainBox as React.CSSProperties}>
+      <div style={styles.mainBox}>
         {renderText()}
-        <Button label={buttonLabel} onClick={onClick} size="large" style={styles.buttonPosition as React.CSSProperties}/>
-			</div>
+        <Button
+          label={buttonLabel}
+          onClick={onClick}
+          size="large"
+          style={styles.buttonPosition}
+        />
+      </div>
       <img
         src={process.env.PUBLIC_URL + IMAGE_BACKGROUND}
         alt=""
@@ -37,28 +39,28 @@ const InfoPage = ({title, caption, onClick, buttonLabel}:InfoPageTypes) => {
   );
 };
 
-const styles = {
+const styles: Styles = {
   mainBox: {
-    position: 'absolute', 
-    top: innerHeight*0.05, 
-    right: innerWidth*0.05, 
-    width: '50%', 
-    display: 'flex', 
-    flexDirection: 'column',
+    position: "absolute",
+    top: innerHeight * 0.05,
+    right: innerWidth * 0.05,
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
     height: innerHeight * 0.45,
-    justifyContent: 'space-between'
+    justifyContent: "space-between",
   },
   buttonPosition: {
-    alignSelf: 'center', 
-    marginTop: 30
+    alignSelf: "center",
+    marginTop: 30,
   },
   titlePosition: {
-    textAlign: 'center'
+    textAlign: "center",
   },
   captionPosition: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 30,
-  }
-} 
+  },
+};
 
 export default InfoPage;
